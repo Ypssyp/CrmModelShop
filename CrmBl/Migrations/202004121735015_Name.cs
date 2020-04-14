b2eaa42e5de803243c9c2c27a0ea0ece53186106
Евгений Евгений <ypssyp1@outlook.com>
2020-04-14 21:55:47 +0300

    Завершил первый урок.

---
+++ CrmBl/Migrations/202004121735015_Name.cs
@@ -0,0 +1,16 @@
+﻿namespace CrmBl.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Name : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ CrmBl/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CrmBl.Model
+{
+   public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { set; get; }
+
+        public int Count { get; set; }
+
+        public virtual ICollection<Sell> Sells { get; set; } 
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+
+    }
+}
